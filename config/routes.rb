a24bac07a003fd321efefc935c2ddfad8bcfbdff
Patrick Miller <pjmiller823@gmail.com>
2017-04-15 11:11:01 -0400

    Added ability to sign in via github, Need to route logout and sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   get 'pages/landing'
 
   root 'pages#landing'
+  get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
+  get    '/auth/:provider/callback' => 'session#create'
+  get    '/auth/failure'            => 'session#failure'
+  
+  get    '/login'                   => 'session#new'
+  post   '/login'                   => 'session#create'
+  get    '/logout'                  => 'session#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414002804) do
+ActiveRecord::Schema.define(version: 20170415132614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20170414002804) do
     t.integer  "created_by_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "nickname"
+    t.string   "access_token"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
