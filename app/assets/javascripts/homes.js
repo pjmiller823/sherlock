a1acdb8e60e61ea69ef2eda3af89e347b96b5bb7
Patrick Miller <pjmiller823@gmail.com>
2017-04-27 20:16:11 -0400

    added js and moved index to partial. error getting dynamic search

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,2 +1,12 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+  $('#query').on('input', function(event) {
+    let queryValue = $(this).val()
+
+    $.ajax({
+      url: '/homes',
+      data: { query: queryValue},
+      datatype: 'script'
+    })
+  })
+})
