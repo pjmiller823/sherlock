b89749a14c986b80c5a8d833b8bde852eab36a5d
Patrick Miller <pjmiller823@gmail.com>
2017-04-28 06:16:44 -0400

    Converted js file from slim to erb

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   $('#query').on('input', function(event) {
     let queryValue = $(this).val()
 
+    console.log(`You are searching for ${queryValue}`)
+
     $.ajax({
       url: '/homes',
       data: { query: queryValue},
