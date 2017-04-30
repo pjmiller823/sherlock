1ffeeecef2d88ef5d38ff43925edff6569a0b16f
Patrick Miller <pjmiller823@gmail.com>
2017-04-30 14:00:47 -0400

    meter to feet trasformation works on show page as well.

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+  // Square Feet to Square Meters
+  $('.show-media').on('click', '.square-footage', function(event) {
+   let sqFeet = $(this).data("sqfeet")
+   let sqMeters = (sqFeet * 0.093).toFixed(2)
+   console.log(`The value is ${sqMeters}`)
+   $(this).replaceWith( `<li class="square-meters" data-sqfeet="${sqFeet}">Meters Squared: ${sqMeters}</li>` )
+  })
 
+  // Square Meters to Square Feet
+  $('.show-media').on('click', '.square-meters', function(event) {
+   let sqFeet = $(this).data("sqfeet")
+   console.log(`The value is ${sqFeet}`)
+   $(this).replaceWith( `<li class="square-footage" data-sqfeet="${sqFeet}">Square Footage: ${sqFeet}</li>` )
+  })
+})
