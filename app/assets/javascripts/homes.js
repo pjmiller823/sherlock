9974821165c30b4b9fc62226ae42ce15504929f5
Patrick Miller <pjmiller823@gmail.com>
2017-04-30 13:53:12 -0400

    Square meters now changes back to square feet

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -42,14 +42,14 @@ $(document).ready(function() {
    let sqFeet = $(this).data("sqfeet")
    let sqMeters = (sqFeet * 0.093).toFixed(2)
    console.log(`The value is ${sqMeters}`)
-   $(this).replaceWith( `<li class="square-meters" data-sqfeet="${sqMeters}">${sqMeters} meters squared</li>` )
+   $(this).replaceWith( `<li class="square-meters" data-sqfeet="${sqFeet}">Meters Squared: ${sqMeters}</li>` )
   })
 
    // Square Meters to Square Feet
-  $('.media').on('click', '.sqMetersFeet', function(event) {
+  $('.media').on('click', '.square-meters', function(event) {
    let sqFeet = $(this).data("sqfeet")
    console.log(`The value is ${sqFeet}`)
-   $(this).replaceWith( `<li class="square-meters" data-sqfeet="${sqFeet}">Square Footage: ${sqFeet}</li>` )
+   $(this).replaceWith( `<li class="square-footage" data-sqfeet="${sqFeet}">Square Footage: ${sqFeet}</li>` )
   })
 
 
